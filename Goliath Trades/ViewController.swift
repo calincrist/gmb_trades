b1015ad0c58ae439c9157bbeb90dedb4f16fd497
Calin Cristian <calin.crist@gmail.com>
2020-03-04 23:13:51 +0200

    Initial Commit

---
+++ Goliath Trades/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goliath Trades
+//
+//  Created by Calin Cristian on 04/03/2020.
+//  Copyright © 2020 Calin Cristian Ciubotariu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
